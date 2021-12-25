cc0039f3603dbc191863a35bf21ae1dc1f20e06d
YoungSandw1ch <sersus1991@gmail.com>
2021-12-25 13:47:00 +0000

    Deploying to gh-pages from @ YoungSandw1ch/team-work-repo@6ce0c6edd533c6be8436fb362027946586f68986 🚀

--- src.77c2db96.js
+++ src.77c2db96.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-work-repo/src.26322fef.js.map
+//# sourceMappingURL=/team-work-repo/src.77c2db96.js.map
